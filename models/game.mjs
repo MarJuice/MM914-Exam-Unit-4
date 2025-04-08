3b1d6759d7bed82312ed849ccb228e6b4ebbef60
MarJuice <mariushanss1@gmail.com>
2025-04-08 23:27:42 +0200

    Step 2

---
+++ models/game.mjs
@@ -0,0 +1,14 @@
+export default class Game {
+    constructor(title, designer, artist, publisher, year, players, time, url, playCount, personalRating) {
+        this.title = title;
+        this.designer = designer;
+        this.artist = artist;
+        this.publisher = publisher;
+        this.year = year;
+        this.players = players;
+        this.time = time;
+        this.url = url;
+        this.playCount = playCount;
+        this.personalRating = personalRating;
+    }
+}
